e06b196424826971def7a9ff94f40e8c592def71
Matthew Lyon <matthew@lyonheart.us>
2020-11-01 22:05:04 -0800

    favicons

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,13 +38,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `matthew lyon`,
+        short_name: `lyonheart`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#f2ece1`,
+        theme_color: `#cc4844`,
         display: `minimal-ui`,
-        // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/assets/favicon.png`
       },
     },
     plugin(`gatsby-plugin-feed`, {
